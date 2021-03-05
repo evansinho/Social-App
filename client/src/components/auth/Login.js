ccae6d5af2575d9f407412e4ea27b898fe51bdc8
evanson <igiri.evanson@gmail.com>
2021-03-05 07:41:26 +0100

    create the login form and state

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,54 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom';
+
 
 const Login = () => {
+  const [ formData, setFormData ] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("success")
+  } 
+
+  const onchange = (e) => setFormData({...formData, [e.target.name]: e.target.value});
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Fragment>
+      <section className="container">
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            required
+            value={email} 
+            onChange={e => onchange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password} 
+            onChange={e => onchange(e)}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </section>
+    </Fragment>
   )
 }
 
