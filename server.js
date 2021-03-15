4c1177ec8b44df48a6e55e2c28ed4a55e47dacb5
evanson <igiri.evanson@gmail.com>
2021-03-15 12:49:09 +0100

    fix a bug

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.json({extended: false}));
 app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(client/build));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
