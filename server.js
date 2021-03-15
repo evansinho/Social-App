114e2239386959e3a419d48e9a1f1b03f7d5fa2b
evanson <igiri.evanson@gmail.com>
2021-03-15 12:03:35 +0100

    prepare app for deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors'
 import dotenv from 'dotenv';
 import routes from './routes/index.js';
+import path from 'path';
 
 dotenv.config();
 const Database = process.env.DATABASE;
@@ -25,10 +26,14 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json({extended: false}));
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome'})
-})
 app.use(routes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(client/build));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 // listener
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
